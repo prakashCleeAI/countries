fbc3d6f2c03b9c3ec8d017a735110aa62a6fc5a3
prakashCleeAI <prakash@cleeai.com>
2024-06-09 14:30:33 +0100

    store countries in a state, and show countries as json in the page.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import "./App.css";
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const REST_COUNTRIES_URL = "https://restcountries.com/v3.1/all";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
   useEffect(() => {
     const fetchCountries = async () => {
       const response = await fetch(REST_COUNTRIES_URL);
       const data = await response.json();
-      console.log(data);
+      setCountries(data);
     };
 
     fetchCountries();
   }, []);
 
-  return <div className="App"></div>;
+  return <div className="App">{JSON.stringify(countries)}</div>;
 }
 
 export default App;
